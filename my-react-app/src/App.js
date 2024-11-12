7c8f94beb3614f9d124244af1a24cb8498b2519b
arranrogerson <87150239+arranrogerson@users.noreply.github.com>
2024-11-12 01:58:50 -0800

    v0.1

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,15 +1,23 @@
 import"./App.css";
-import { add, multiply, subtract, divide} from "./calculator.js";
+import Heading from "./Heading";
 
 function App () {
   return (
-      <ul>
-        <li>{add(1, 2)}</li>
-        <li>{multiply(2, 3)}</li>
-        <li>{subtract(7, 2)}</li>
-        <li>{divide(5, 2)}</li>
-      </ul>
+      <div>
+        <Heading />
+      </div>
     )
 }
 
 export default App;
+
+//3. Create a Header.jsx component that renders a <header> element
+//to show the Keeper App name in an <h1>.
+//4. Create a Footer.jsx component that renders a <footer> element
+//to show a copyright message in a <p> with a dynamically updated year.
+//5. Create a Note.jsx component to show a <div> element with a
+//<h1> for a title and a <p> for the content.
+//6. Make sure that the final website is styled like the example shown here:
+//https://l1pp6.csb.app/
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
